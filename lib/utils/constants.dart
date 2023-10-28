8290bd869f6f2d170efacdb3c09f3bb74f09d6c2
makecodework <makecodework@gmail.com>
2023-10-29 00:18:43 +0300

    start

---
+++ lib/utils/constants.dart
@@ -0,0 +1,24 @@
+// utils/constants.dart
+
+class AppConstants {
+  // Base API URL
+  static const String BASE_API_URL = "https://yourserver.com/api";
+
+  // App name
+  static const String APP_NAME = "MediApp";
+
+  // Colors
+  static const PRIMARY_COLOR = 0xFF6200EE;
+  static const ACCENT_COLOR = 0xFF03DAC5;
+
+  // Route names
+  static const String LOGIN = "/login";
+  static const String REGISTRATION = "/registration";
+  static const String DASHBOARD = "/dashboard";
+  static const String SUPER_ADMIN_PAGE = "/super_admin";
+  static const String ADMIN_PAGE = "/admin";
+  static const String NEUROPSYCHOLOGIST_PAGE = "/neuropsychologist";
+  static const String PHYSICAL_THERAPIST_PAGE = "/physical_therapist";
+  static const String OCCUPATIONAL_THERAPIST_PAGE = "/occupational_therapist";
+  static const String PARTICIPANT_PAGE = "/participant";
+}
